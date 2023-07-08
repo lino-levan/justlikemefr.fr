1273aa6b10ae19626aafe95f56fe43586127cd8b
lino-levan <11367844+lino-levan@users.noreply.github.com>
2023-07-08 15:49:07 -0700

    He's just like me fr fr for twitter

--- routes/index.tsx
+++ routes/index.tsx
@@ -9,12 +9,16 @@ export default function Home() {
         <meta content="He's just like me fr fr" property="og:description" />
         <meta content="https://justlikemefr.fr" property="og:url" />
         <meta content="https://justlikemefr.fr/img.jpg" property="og:image" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="He's just like me fr fr" />
+        <meta name="twitter:description" content="He's just like me fr fr" />
+        <meta name="twitter:image" content="https://justlikemefr.fr/img.jpg" />
       </Head>
       <img
-          src="/img.jpg"
-          class="w-full"
-          alt="he's just like me fr fr"
-        />
+        src="/img.jpg"
+        class="w-full"
+        alt="he's just like me fr fr"
+      />
     </>
   );
 }
